766fde5e455fb992179937fc25a3d6ed949b1d5a
Pedro Cesar <119058413+PedroCesar-Dev@users.noreply.github.com>
2023-12-28 17:01:32 -0300

    Update

--- ContagemRegressiva/src/App.jsx
+++ ContagemRegressiva/src/App.jsx
@@ -1,18 +1,22 @@
 import AppTitulo from './components/AppTitulo'
 import Newyear from './assets/background.jpg'
 import Contador from './components/contador'
+import useCountDown from './hooks/userCountDown'
 import './App.css'
 
+
 function App() {
+  const [day, hour, minute, second] = useCountDown ('Jan 1, 2024 00:00:00')
   return (
+    
     <div className="App" style ={{backgroundImage:`url(${Newyear})`}}>
       <div className='container'>
        <AppTitulo apptitulo="Contagem Regressiva"/>
       <div className='contagem-regressiva'>
-        <Contador title="Dias" number={2}/>
-        <Contador title="Horas" number={2}/>
-        <Contador title="Minutos" number={2}/>
-        <Contador title="Segundos" number={2}/>
+        <Contador title="Dias" number={day}/>
+        <Contador title="Horas" number={hour}/>
+        <Contador title="Minutos" number={minute}/>
+        <Contador title="Segundos" number={second}/>
       </div>
       </div>
     </div>
